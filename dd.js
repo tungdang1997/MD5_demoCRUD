2b07bf0d62fd8635d81fdf64475b566ede633f78
dangtung <tungdang1997@example.com>
2023-02-13 15:41:32 +0700

    CRUD

---
+++ dd.js
@@ -0,0 +1,11 @@
+import {Component} from "react";
+
+export default class D extends Component{
+    render() {
+        return (
+            <h1>
+                Hi
+            </h1>
+        );
+    }
+}
